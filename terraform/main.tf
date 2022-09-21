1cfd263feb0750bda3c0dca692d00aba2e8df4a9
Kazuki_Kamizuru <k.amago0721@gmail.com>
2022-09-21 23:03:14 +0900

    chore: update version of hashicorp/aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_version = "~> 1.0"
-
-  backend "remote" {
-    organization = "k2ss"
-
-    workspaces {
-      name = "atmark-infra"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.34.0"
-    }
-  }
-}
-
 provider "aws" {
   profile = var.profile
   region  = var.region
